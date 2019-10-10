06eb3b58201971071625d5cbd021eb90965c8570
and <33139756+Im-Andrew@users.noreply.github.com>
2019-10-09 23:40:43 -0700

    include rest

---
+++ src/cse360assign2/AddingMachineTest.java
@@ -0,0 +1,65 @@
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddingMachineTest {
+
+	@Test
+	void test_cancels_out() {
+		var machine = new AddingMachine();
+		machine.add(20);
+		machine.add(-20);		
+		assertTrue( machine.getTotal() == 0 );
+	}
+	
+	@Test
+	void test_clear() {
+		var machine = new AddingMachine();
+		machine.add(20);
+		assertTrue( machine.getTotal() == 20 );
+		machine.clear();
+		assertTrue( machine.getTotal() == 0 );
+	}
+	
+	@Test
+	void test_subtracting_negatives() {
+		var machine = new AddingMachine();
+		machine.subtract(-20);
+		machine.subtract(-40);
+		machine.subtract(-10);
+		assertTrue( machine.getTotal() == 70 );
+		machine.clear();
+	}	
+	
+	@Test
+	void test_string_output() {
+		var machine = new AddingMachine();
+		machine.subtract(-20);
+		machine.add(-10);
+		machine.add( 30 );
+		machine.subtract(0);
+		machine.subtract(10);
+		machine.add(10);
+
+		assertTrue( machine.toString().equals("0 + 20 - 10 + 30 - 0 - 10 + 10") );
+	}	
+
+	
+	@Test
+	void test_final() {
+		var machine = new AddingMachine();
+		machine.subtract(-20);
+		machine.add(-10);
+		machine.add( 30 );
+		machine.subtract(0);
+		machine.subtract(10);
+		machine.add(10);
+		
+		assertTrue( machine.getTotal() == (20 + -10 + 30 - 0 - 10 + 10) );
+	}	
+	
+	
+
+}
